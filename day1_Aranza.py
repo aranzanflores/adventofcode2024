ab151230ab36071f6f0ad3a3865e3361daebc041
aranza.flores <aranza.flores@volvocars.com>
2024-12-01 21:26:34 +0100

    Modularization of Day1 code

--- day1_Aranza.py
+++ day1_Aranza.py
@@ -1,29 +1,45 @@
-list1 =  []
-list2 =  []
-
-with open('./input.txt') as file_handler:
-    for line in file_handler:
-        result = line.split('   ')
-        if (result[0] != ""):
+def filter_input():
+    list1 = []
+    list2 = []
+    with open('./input.txt', encoding='utf-8') as file_handler:
+        for line in file_handler:
+            result = line.split('   ')
             list1.append(int(result[0]))
-        if (result[1] != ""):
             list2.append(int(result[1]))
-list1.sort()
-list2.sort()
-distance = 0
-index = 0
-similarityindex = 0
+    return list1, list2
 
-listrepeatednumbers = []
-for number in list1:
-    distance = distance + abs(number - list2[index])
-    if (number in list2) & (number not in listrepeatednumbers):
+
+def calculate_similarity_index(number: int, list1: list, list2: list, numbersalreadycalculated: list) -> int:
+    if number not in list2:
+        return 0
+    if number in numbersalreadycalculated:
+        return 0
+    repeatedinstances = list2.count(number)
+    similarityindex = number * repeatedinstances
+    return similarityindex
+
+def calculate_distance_and_similarity_index(list1: list, list2: list):
+    index = 0
+    distance = 0
+    similarityindex = 0
+    listrepeatednumbers = []
+    for number in list1:
+        distance += abs(number - list2[index])
+        index += 1
+        calculated_similarity_index = calculate_similarity_index(number, list1, list2, listrepeatednumbers) 
+        if calculated_similarity_index == 0:
+            continue
         listrepeatednumbers.append(number)
-        repeatedinstances = list2.count(number)
-        similarityindex = similarityindex + (number * repeatedinstances)
-    index = index + 1
+        similarityindex += calculated_similarity_index
+    return distance, similarityindex
 
+def main():
+    list1, list2 = filter_input()
+    list1.sort()
+    list2.sort()
+    distance, similarityindex = calculate_distance_and_similarity_index(list1, list2)
+    print(distance)
+    print(similarityindex)
 
-print(index)
-print(distance)
-print(similarityindex)
+if __name__ == "__main__":
+    main()
